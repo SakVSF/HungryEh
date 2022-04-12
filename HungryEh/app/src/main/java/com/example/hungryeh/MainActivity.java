e11b46d5100de743339a99e8e4b5c3711ef640f5
SAAKSHI VINOD SARAF <76583844+SakVSF@users.noreply.github.com>
2022-04-12 15:15:33 +0800

    Update MainActivity.java

--- HungryEh/app/src/main/java/com/example/hungryeh/MainActivity.java
+++ HungryEh/app/src/main/java/com/example/hungryeh/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.hungryeh;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
 
 
 
@@ -19,18 +20,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
         Button login_button = (Button) findViewById(R.id.login);
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(v == login_button)
-                {
-                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                    startActivity(intent);
+                {   FirebaseUser currentUser = mAuth.getCurrentUser();
+                    if(currentUser == null) {
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Intent intent = new Intent(MainActivity.this, HomePageActivity.class);
+                        startActivity(intent);
+                    }
 
                 }
-            }
-        });
+
+                }
+            });
     }
 }
 

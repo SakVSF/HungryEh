96a57aab1cfdb8f8eb185d015b9ee2ab91e889da
rmurarishetti <64725000+rmurarishetti@users.noreply.github.com>
2022-04-23 23:48:11 +0800

    Commented CartActivity

--- HungryEh/app/src/main/java/com/example/hungryeh/CartActivity.java
+++ HungryEh/app/src/main/java/com/example/hungryeh/CartActivity.java
@@ -45,43 +45,23 @@ public class CartActivity extends AppCompatActivity {
         firestore = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
         cartItem = new ArrayList<CartItem>();
-        cartAdapter = new CartAdapter(CartActivity.this, cartItem);
+        cartAdapter = new CartAdapter(CartActivity.this, cartItem); //initializing cartadapter for recyclerview
 
         recyclerView.setAdapter(cartAdapter);
-        EventChangeListener();
+        EventChangeListener(); //actively checking for changes in the firebase to add items into the cartview
 
 
         paynow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //payment button leading to the payment page
                 startActivity(new Intent(CartActivity.this, PaymentActivity.class));
             }
         });
     }
 
-    private void getCartDetails(){
-        firestore.collection("cartItems").document(auth.getCurrentUser().getUid()).collection("Mycart").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if (error != null) {
-                    Log.e("Firestore error", error.getMessage());
-                    return;
-                }
-                for(DocumentChange dc: value.getDocumentChanges()){
-                    if (dc.getType() == DocumentChange.Type.ADDED){
-                        cartItem.add(dc.getDocument().toObject(CartItem.class));
-                    }
-                }
-
-            }
-        });
-
-    }
-
-    private void initializePaymentPage() {
-        //logic to see if there is any cartitem
-
-        //if cartitem has 1 or more item then calculate total
+    private void initializePayment() {
+        // to display cart total on the payment button on cart
         OverallTotalPrice = 0;
         for ( CartItem item : cartItem ) {
             OverallTotalPrice += item.totalprice;
@@ -99,12 +79,12 @@ public class CartActivity extends AppCompatActivity {
                }
                for(DocumentChange dc: value.getDocumentChanges()){
                    if (dc.getType() == DocumentChange.Type.ADDED){
-                       cartItem.add(dc.getDocument().toObject(CartItem.class));
+                       cartItem.add(dc.getDocument().toObject(CartItem.class)); //adding items to arraylist whenever items are added into cart database
                    }
-                   cartAdapter.notifyDataSetChanged();
+                   cartAdapter.notifyDataSetChanged();//notifying change whenever items are added into the cart database
 
                }
-
+               initializePayment(); //recalculates total everytime EventChangeListener is called
 
             }
         });
